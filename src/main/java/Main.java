44be0c8ccc7ac9a8947259e106cc3bae5e4a398b
Hasan Rafi <mr.k779@outlook.com>
2015-11-22 20:40:46 +0600

    Organizing imports

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,4 @@
 import Controllers.RegistrationCourseController;
-import Models.Course;
-import spark.Filter;
-import spark.Request;
-import spark.Response;
-
-import java.util.LinkedList;
 
 import static Utilities.JsonUtil.json;
 import static spark.Spark.*;
@@ -53,13 +47,10 @@ public class Main {
     }
 
     private static void enableCORS(final String origin, final String methods, final String headers) {
-        before(new Filter() {
-            @Override
-            public void handle(Request request, Response response) {
-                response.header("Access-Control-Allow-Origin", origin);
-                response.header("Access-Control-Request-Method", methods);
-                response.header("Access-Control-Allow-Headers", headers);
-            }
+        before((request, response) -> {
+            response.header("Access-Control-Allow-Origin", origin);
+            response.header("Access-Control-Request-Method", methods);
+            response.header("Access-Control-Allow-Headers", headers);
         });
     }
 
